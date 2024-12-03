7ce86080c809793107c4ddc8618952cf5b8bbf5e
Aadem Isai <145148144+CollegeAadem@users.noreply.github.com>
2024-12-03 10:59:04 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // main.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
 import { firebaseConfig } from './firebase-config.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 document.getElementById('login-form').addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -22,6 +24,7 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
         const user = userCredential.user;
 
         console.log('User logged in:', user);
+
         window.location.href = 'mainfeed.html';
     } catch (error) {
         console.error('Error logging in:', error);
